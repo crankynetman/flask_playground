8a241fbcceb9dc54b80441b2687aae6f08af13aa
cummings-chris <chris@cummings.tech>
2021-01-09 19:55:26 -0600

    Loops in j2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Chris Cummings'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie exists and I have no opinion on the quality of the film!'
+        },
+        {
+            'author': {'username': 'Sarah'},
+            'body': 'This is a message.'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
